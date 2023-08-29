c99de07f36d13afd35df410bf4e400e377755aab
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2023-08-29 20:55:32 +0530

    fix: long params replaced with payload object

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { io, Socket } from 'socket.io-client';
 
+type SendMessagePayload = {
+  text: string;
+  to: string;
+  payload: {
+      userId?: string;
+      [key: string]: any; 
+  };
+};
+
 class UCI {
   socket: Socket | undefined;
   msgReceiveCb: any;
@@ -20,32 +29,43 @@ class UCI {
     this.msgReceiveCb(message);
   };
 
-  handleSocketSession = (session:any) => {
+  handleSocketSession = (session: any) => {
     this.session = session;
   };
 
   onDisconnect = () => {
     this.socket?.disconnect()
   }
-  
 
-  sendMessage = ({ text, to, from, optional, conversationId, asrId, userId }: any) => {
+
+  // sendMessage = ({ text, to, from, optional, conversationId, asrId, userId }: any) => {
+  //   this.socket?.emit("botRequest", {
+  //     content: {
+  //       text,
+  //       to,
+  //       appId: optional?.appId,
+  //       channel: optional?.channel,
+  //       conversationId: conversationId || null, 
+  //       asrId: asrId || null,
+  //       from,
+  //       userId: userId || this.session.userID,
+  //       context: null,
+  //       accessToken: null,
+  //     },
+  //     to,
+  //   });
+  // };
+
+  sendMessage = ( {text, to, payload}: SendMessagePayload) => {
     this.socket?.emit("botRequest", {
       content: {
         text,
         to,
-        appId: optional?.appId,
-        channel: optional?.channel,
-        conversationId: conversationId || null, 
-        asrId: asrId || null,
-        from,
-        userId: userId || this.session.userID,
-        context: null,
-        accessToken: null,
-      },
-      to,
-    });
-  };
+        ...payload,
+        userId: payload.userId || this.session.userID,
+      }
+    }, to)
+  }
 }
 
 export { UCI };
